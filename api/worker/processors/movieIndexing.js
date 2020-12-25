042b2d2658511653aa26f2864d9732162f74346f
Kevin Lowe <iamkevinlowe@gmail.com>
2020-12-25 13:10:48 -0800

    Created ES cluster

--- api/worker/processors/movieIndexing.js
+++ api/worker/processors/movieIndexing.js
@@ -1,18 +1,26 @@
 const Movie = require('../../classes/Movie');
-const esClient = require('../../classes/Elasticsearch');
+const Elasticsearch = require('../../classes/Elasticsearch');
 const { movieIndexQueue } = require('../queues');
+
+const esClient = new Elasticsearch({ node: process.env.ES_HOST });
 
 module.exports = async (job, done) => {
 	const movie = new Movie(job.data);
-	const response = await esClient.request('get', { index: Movie.INDEX, id: movie.id });
 
-	if (response && response.found) {
-		done(null, `Found ${movie.title}`);
-	} else {
-		await movie.fetchAdditionalDetails();
-		movie.similar.forEach(movie => movieIndexQueue.add(movie));
-		movie.recommendations.forEach(movie => movieIndexQueue.add(movie));
-		await esClient.request('index', { index: Movie.INDEX, id: movie.id, body: movie });
-		done(null, `Indexed ${movie.title}`);
+	try {
+		const response = await esClient.request('get', { index: Movie.INDEX, id: movie.id });
+
+		if (response && response.found) {
+			done(null, `Found ${movie.title}`);
+		} else {
+			await movie.fetchAdditionalDetails();
+			movie.similar.forEach(movie => movieIndexQueue.add(movie));
+			movie.recommendations.forEach(movie => movieIndexQueue.add(movie));
+			await esClient.request('index', { index: Movie.INDEX, id: movie.id, body: movie });
+			done(null, `Indexed ${movie.title}`);
+		}
+	} catch (e) {
+		console.error('Failed indexing movie', e);
+		done(e.message);
 	}
 };
