da5c36b87ba08682dafac57b5b2ebd851400e502
Kevin Lowe <iamkevinlowe@gmail.com>
2021-01-11 06:29:01 -0800

    Moving es

--- app/classes/Elasticsearch.js
+++ app/classes/Elasticsearch.js
@@ -1,5 +1,5 @@
 const { Client } = require('@elastic/elasticsearch');
-const { capitalize } = require('../classes/UtilString');
+const { capitalize } = require('../utils/UtilString');
 
 class Elasticsearch {
 	/**
